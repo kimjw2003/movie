2cf58125e671684223b02fa0204d08268661a1b1
kimjw2003 <kimjw111003@gmail.com>
2020-10-29 11:02:59 +0900

    제목 수정

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.movie
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.example.movie.data.Base
 import com.example.movie.retrofit.MovieClient
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,17 +26,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun getMovie(){
         MovieClient.retrofitService.getMovie("kmdb_new2", "Y", "1QS3HYA074P8X6W4TEF3", "대한민국",
-            "20201028", ""+search_Et.text.toString(), ""
+            "20201015", ""+search_Et.text.toString(), ""
         ).enqueue(object : retrofit2.Callback<Base> {
+            @SuppressLint("CheckResult")
             override fun onResponse(call: Call<Base>, response: Response<Base>) {
                 Log.d("Logd", "good")
 
+                var poster:String = response.body()?.Data?.get(0)?.Result?.get(0)?.posters!!
 
+                var idx:Int = poster.indexOf("|")
+                Log.d("Logd", poster.substring(0, idx))
 
-                title_Tv.text = response.body()?.KMAQuery
+                Glide.with(poster_Iv)
+                    .load(poster.substring(0, idx))
+                    .into(poster_Iv)
+
+                var idx2:Int = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.indexOf("^")
+
+                title_Tv.text = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.substring(0, idx2)
                 actor_Tv.text = response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(0)?.actorNm
-                actor2_Tv.text = response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(1)?.actorNm
-
+                if(response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.size!! > 1) {
+                    actor2_Tv.text =
+                        response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(1)?.actorNm
+                }
             }
 
             override fun onFailure(call: Call<Base>, t: Throwable) {
