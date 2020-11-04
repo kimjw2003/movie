946e920c092d038341635f504464949f5a4ee966
kimjw2003 <kimjw111003@gmail.com>
2020-11-05 08:46:10 +0900

    [UPDATE] search

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -33,6 +33,8 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<Base>, response: Response<Base>) {
                 Log.d("Logd", "good")
 
+                //제목 ^앞까지만 보여주라고 설정하기
+                var idx2:Int = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.indexOf("^")
                 // 포스터 선언하기
                 var poster:String = response.body()?.Data?.get(0)?.Result?.get(0)?.posters!!
                 // 포스터 1개만 보여주기
@@ -41,45 +43,45 @@ class MainActivity : AppCompatActivity() {
                 // 만약에 검색결과 영화가 1개면
                 if(response.body()?.TotalCount.toString() == "1") {
                 Log.d("Logd", "movie 개수 = 1")
-                // 포스터 적용
-                Glide.with(poster_Iv)
-                    .load(poster.substring(0, idx))
-                    .into(poster_Iv)
-
-
-
-                //제목 ^앞까지만 보여주라고 설정하기
-                var idx2:Int = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.indexOf("^")
-                // 제목 보여주기
-                title_Tv.text = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.substring(0, idx2)
 
                     //리사이클러뷰 리스트 시1발
                     var list = ArrayList<MovieData>()
                     list.add(MovieData(response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.substring(0, idx2),
                         response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(0)?.actorNm!!, poster.substring(0, idx)))
-
                     val adapter = RcViewAdapter(list)
                     rcView.adapter = adapter
                     rcView.layoutManager = LinearLayoutManager(this@MainActivity)
 
-
-                // 주연배우
-                actor_Tv.text = response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(0)?.actorNm
-                //주연배우가 2명이상일 경우 if문 실행
                 if(response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.size!! > 1) {
-                    actor2_Tv.text =
-                        response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(1)?.actorNm
+
                     }
                 }else {
                     Log.d("Logd","검색된 영화 수 : "+ response.body()?.TotalCount.toString())
-                }
 
+                    var list = ArrayList<MovieData>()
+                    response.body()!!.Data!!.forEach {data ->
+                        data.Result?.forEach {
+                            var postIdx = it.posters.indexOf("|")
+                            if(postIdx == -1){
+                                postIdx = it.posters.length
+                            }
+
+                            list.add(MovieData(it.titleEtc!!.substring(0, it.titleEtc!!.indexOf("^")),
+                                it.actors?.actor?.get(0)?.actorNm!!,
+                                if(it.posters.isNotEmpty()) it.posters.substring(0, postIdx) else ""))
+                        }
+                    }
+
+
+                    val adapter = RcViewAdapter(list)
+                    rcView.adapter = adapter
+                    rcView.layoutManager = LinearLayoutManager(this@MainActivity)
+                }
             }
 
             override fun onFailure(call: Call<Base>, t: Throwable) {
                 Log.d("Logd", t.message.toString())
             }
-
         })
     }
 }
