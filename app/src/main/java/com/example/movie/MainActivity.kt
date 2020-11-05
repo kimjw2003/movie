3754ab1b7592fc7e711bc48bff5d0663bb2f325d
kimjw2003 <kimjw111003@gmail.com>
2020-11-05 09:46:15 +0900

    [UPDATE] 코드 줄이기

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -31,52 +31,24 @@ class MainActivity : AppCompatActivity() {
         ).enqueue(object : retrofit2.Callback<Base> {
             @SuppressLint("CheckResult")
             override fun onResponse(call: Call<Base>, response: Response<Base>) {
-                Log.d("Logd", "good")
+                Log.d("Logd", "onResponse")
 
-                //제목 ^앞까지만 보여주라고 설정하기
-                var idx2:Int = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.indexOf("^")
-                // 포스터 선언하기
-                var poster:String = response.body()?.Data?.get(0)?.Result?.get(0)?.posters!!
-                // 포스터 1개만 보여주기
-                var idx:Int = poster.indexOf("|")
-
-                // 만약에 검색결과 영화가 1개면
-                if(response.body()?.TotalCount.toString() == "1") {
-                Log.d("Logd", "movie 개수 = 1")
-
-                    //리사이클러뷰 리스트 시1발
-                    var list = ArrayList<MovieData>()
-                    list.add(MovieData(response.body()?.Data?.get(0)?.Result?.get(0)?.titleEtc!!.substring(0, idx2),
-                        response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.get(0)?.actorNm!!, poster.substring(0, idx)))
-                    val adapter = RcViewAdapter(list)
-                    rcView.adapter = adapter
-                    rcView.layoutManager = LinearLayoutManager(this@MainActivity)
-
-                if(response.body()?.Data?.get(0)?.Result?.get(0)?.actors?.actor?.size!! > 1) {
-
-                    }
-                }else {
                     Log.d("Logd","검색된 영화 수 : "+ response.body()?.TotalCount.toString())
 
-                    var list = ArrayList<MovieData>()
-                    response.body()!!.Data!!.forEach {data ->
-                        data.Result?.forEach {
+                    var movieList = ArrayList<MovieData>()
+                        response.body()?.Data?.get(0)?.Result?.forEach {it ->       //response.body()?.Data?.get(0)?.Result -> it 으로 정의
                             var postIdx = it.posters.indexOf("|")
                             if(postIdx == -1){
                                 postIdx = it.posters.length
                             }
-
-                            list.add(MovieData(it.titleEtc!!.substring(0, it.titleEtc!!.indexOf("^")),
+                            movieList.add(MovieData(it.titleEtc!!.substring(0, it.titleEtc!!.indexOf("^")),
                                 it.actors?.actor?.get(0)?.actorNm!!,
                                 if(it.posters.isNotEmpty()) it.posters.substring(0, postIdx) else ""))
-                        }
                     }
 
-
-                    val adapter = RcViewAdapter(list)
+                    val adapter = RcViewAdapter(movieList)
                     rcView.adapter = adapter
                     rcView.layoutManager = LinearLayoutManager(this@MainActivity)
-                }
             }
 
             override fun onFailure(call: Call<Base>, t: Throwable) {
