eb563fc87c7fd6cd64eddf5bfade0a51b6a98595
kimjw2003 <kimjw111003@gmail.com>
2020-11-07 22:08:19 +0900

    [ADD] 영어제목, 년도

--- app/src/main/java/com/example/movie/DetailActivity.kt
+++ app/src/main/java/com/example/movie/DetailActivity.kt
@@ -3,7 +3,13 @@ package com.example.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.example.movie.data.Base
+import com.example.movie.retrofit.MovieClient
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class DetailActivity : AppCompatActivity() {
 
@@ -24,6 +30,28 @@ class DetailActivity : AppCompatActivity() {
             .into(detail_poster)
 
         detail_title.text = title.toString()
-        detail_actor.text = actor.toString()
+       // detail_actor.text = actor.toString()
+
+        getDetailMovie()
+    }
+
+    fun getDetailMovie(){
+        MovieClient.retrofitService.getMovie("kmdb_new2", "Y", "1QS3HYA074P8X6W4TEF3", "",
+            ""+detail_title.text.toString(), "", "극영화", "1")
+            .enqueue(object : retrofit2.Callback<Base>{
+                override fun onResponse(call: Call<Base>, response: Response<Base>) {
+
+                    var titleEng_idx: Int = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEng!!.indexOf("(")
+
+                    //detail_director.text = response.body()?.Data?.get(0)?.Result?.get(0)?.directors?.director?.get(0)?.directorNm
+                    detail_titleEng.text = response.body()?.Data?.get(0)?.Result?.get(0)?.titleEng!!.substring(0, titleEng_idx)
+                    detail_openDate.text = response.body()?.Data?.get(0)?.Result?.get(0)?.repRlsDate
+                }
+
+                override fun onFailure(call: Call<Base>, t: Throwable) {
+
+                }
+
+            })
     }
 }
